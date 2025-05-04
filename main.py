147b532c031e4565cdbdb150303dd2466a266524
SEEPANA MEGHANA <meghana2805@gmail.com>
2025-05-04 12:07:05 +0530

    Model evaluation pipeline added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from winequality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPi
 from winequality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from winequality.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from winequality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from winequality.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
 STAGE_NAME = "Data Ingestion stage"
 
 try:
@@ -44,6 +46,18 @@ STAGE_NAME = "Model Trainer Stage"
 try:
     logger.info(f"=======stage {STAGE_NAME} started ========")
     obj = ModelTrainerTrainingPipeline()
+    obj.main()
+    logger.info(f"=======stage {STAGE_NAME} completed ========\n\nx=========x")
+
+except Exception as e:
+    logger.exception(e)    
+    raise e 
+
+STAGE_NAME = "Model Evaluation Stage"
+
+try:
+    logger.info(f"=======stage {STAGE_NAME} started ========")
+    obj = ModelEvaluationTrainingPipeline()
     obj.main()
     logger.info(f"=======stage {STAGE_NAME} completed ========\n\nx=========x")
 
